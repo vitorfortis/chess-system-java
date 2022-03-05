fe63be008da75daa08d7b8ca2bcee54a5b909c15
Vitor Alexandre Paula da Silva <85693835+vitorfortis@users.noreply.github.com>
2022-03-04 23:46:57 -0300

    Possible moves of a piece

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -44,6 +44,12 @@ public class ChessMath {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no  Piece on source position");
 		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			
+			throw new ChessException("There is no  Possible moves for the chosen piece");
+			
+			
+		}
 	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece ) {
