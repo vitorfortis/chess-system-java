c66a552a69b0616d63de3a22ec7a305f32653fdb
Vitor Alexandre Paula da Silva <85693835+vitorfortis@users.noreply.github.com>
2022-04-04 18:43:08 -0300

    en passant part2

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -46,7 +46,7 @@ public class Pawn extends ChessPiece {
 					mat[left.getRow() -1][left.getColumn()] = true;
 				}
 				Position right = new Position(position.getRow(), position.getColumn()+1);
-				if(getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(left) == chessMath.getEnPassantVulnerable()) {
+				if(getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMath.getEnPassantVulnerable()) {
 					mat[right.getRow() -1][right.getColumn()] = true;
 				}
 			}
